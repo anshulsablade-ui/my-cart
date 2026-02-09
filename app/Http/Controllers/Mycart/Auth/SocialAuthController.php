426e4733240d6a5a23498170fbb6dd61a67b4645
Anshul <anshulsablade@gmail.com>
2026-02-09 17:14:42 +0530

    Fix cart count update on user login in SocialAuthController

--- app/Http/Controllers/Mycart/Auth/SocialAuthController.php
+++ app/Http/Controllers/Mycart/Auth/SocialAuthController.php
@@ -29,6 +29,7 @@ class SocialAuthController extends Controller
         if ($user) {
             session()->put('user', $user);
             $this->gaustCartMerge();
+            Session::put('cart_count', Cart::where('user_id', $user->id)->count());
             session()->flash('success', 'Login successful!');
             return redirect()->route('home');
         }
